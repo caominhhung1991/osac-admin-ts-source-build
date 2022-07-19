ae420d6b987b3eb721aefa5ce395a5050f172891
Hung Cao <hungcao@Hungs-MacBook-Pro.local>
2022-07-19 16:43:08 +0700

    update

--- static/js/95.fdbc4325.chunk.js
+++
@@ -1,1 +0,0 @@
-(this["webpackJsonpreact-antd-admin"]=this["webpackJsonpreact-antd-admin"]||[]).push([[95],{1059:function(c,t,a){"use strict";a.r(t);var e=a(0),n=a.n(e),s=a(50),b=a(48),i=a(269),r=a(449),u=a(17);t.default=function(){var c=Object(b.b)();return n.a.useEffect((function(){c(Object(r.c)())}),[]),Object(u.b)("div",{className:"bHXH-page",css:i.a,children:Object(u.b)(s.c,{})})}}}]);
